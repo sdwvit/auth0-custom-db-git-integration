35890c0aeff8ecda9fae359c71cea529ac6322f4
Victor Musiienko <victor.musiienko@auth0.com>
2022-09-14 17:47:37 -0400

    try to push custom scripts

--- .onpush.js
+++ .onpush.js
@@ -1,23 +1,35 @@
 console.log("hello from gh action");
 
-const customScriptFilenames = [
-  "changePassword",
-  "create",
-  "delete",
-  "getByEmail",
-  "login",
-  "verify",
-];
-
 const DOMAIN = process.env.DOMAIN;
+const CONNECTION_ID = process.env.CONNECTION_ID;
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-console.log({ DOMAIN, CLIENT_ID: CLIENT_ID ? 'set' : 'not set', CLIENT_SECRET: CLIENT_SECRET ? 'set' : 'not set' });
+console.log({
+  DOMAIN: DOMAIN ? "set" : "not set",
+  CLIENT_ID: CLIENT_ID ? "set" : "not set",
+  CLIENT_SECRET: CLIENT_SECRET ? "set" : "not set",
+});
 
-const ManagementClient = require('auth0').ManagementClient;
+const ManagementClient = require("auth0").ManagementClient;
 const auth0 = new ManagementClient({
   domain: DOMAIN,
   clientId: CLIENT_ID,
   clientSecret: CLIENT_SECRET,
-  scope: 'read:users update:users',
+  scope: "update:connections",
 });
+
+const customScripts = {
+  change_password: require("./changePassword.js"),
+  create: require("./create.js"),
+  delete: require("./delete.js"),
+  get_user: require("./getByEmail.js"),
+  login: require("./login.js"),
+  verify: require("./verify.js"),
+};
+
+auth0.connections.update(
+  { id: CONNECTION_ID },
+  {
+    customScripts: customScripts,
+  }
+);
